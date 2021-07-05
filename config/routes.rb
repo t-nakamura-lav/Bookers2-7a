bba5410690856b3a3322fedf2187f0ea00ddc6ca
EC2 Default User <ec2-user@ip-172-31-13-63.ec2.internal>
2021-07-05 12:45:28 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+
+end
